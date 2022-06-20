ab7e9c354ec62a071fe3394572694e6437fd4517
Kais Dukes <dukeskais79@gmail.com>
2022-06-20 20:37:12 +0100

    Sliding windows.

--- src/main/java/adventofcode2021/day1/sonarsweep/SonarSweep.java
+++ src/main/java/adventofcode2021/day1/sonarsweep/SonarSweep.java
@@ -1,5 +1,6 @@
 package adventofcode2021.day1.sonarsweep;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SonarSweep {
@@ -10,5 +11,17 @@ public class SonarSweep {
             if (depths.get(i + 1) > depths.get(i)) count++;
         }
         return count;
+    }
+
+    public static List<Integer> slidingWindows(final List<Integer> depths) {
+        final var windows = new ArrayList<Integer>();
+        var sum = depths.get(0) + depths.get(1) + depths.get(2);
+        windows.add(sum);
+
+        for (var i = 0; i < depths.size() - 3; i++) {
+            sum += depths.get(i + 3) - depths.get(i);
+            windows.add(sum);
+        }
+        return windows;
     }
 }
--- src/test/java/adventofcode2021/day1/sonarsweep/SonarSweepTests.java
+++ src/test/java/adventofcode2021/day1/sonarsweep/SonarSweepTests.java
@@ -5,13 +5,14 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.List;
 
-import static adventofcode2021.day1.sonarsweep.SonarSweep.depthIncreases;
+import static adventofcode2021.day1.sonarsweep.SonarSweep.*;
 import static adventofcode2021.io.Io.readInts;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class SonarSweepTests {
+    private static final String REPORT_FILE = "data/day1/sonarsweep.txt";
 
     @Test
     public void shouldReturnDepthIncreases() {
@@ -23,7 +24,28 @@ public class SonarSweepTests {
     @Test
     public void shouldReturnDepthIncreasesForSonarSweepReport() throws IOException {
         assertThat(
-                depthIncreases(readInts("data/day1/sonarsweep.txt")),
+                depthIncreases(readInts(REPORT_FILE)),
                 is(equalTo(1387)));
+    }
+
+    @Test
+    public void shouldReturnSlidingWindows() {
+        assertThat(
+                slidingWindows(List.of(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)),
+                is(equalTo(List.of(607, 618, 618, 617, 647, 716, 769, 792))));
+    }
+
+    @Test
+    public void shouldReturnDepthIncreasesOfSlidingWindows() {
+        assertThat(
+                depthIncreases(slidingWindows(List.of(199, 200, 208, 210, 200, 207, 240, 269, 260, 263))),
+                is(equalTo(5)));
+    }
+
+    @Test
+    public void shouldReturnDepthIncreasesOfSlidingWindowsForSonarSweepReport() throws IOException {
+        assertThat(
+                depthIncreases(slidingWindows(readInts(REPORT_FILE))),
+                is(equalTo(1362)));
     }
 }
