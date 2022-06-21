4d3f3c727d8e39109645af6141dc81922c248481
Kais Dukes <dukeskais79@gmail.com>
2022-06-21 14:00:09 +0100

    Stateless diagnostic report.

--- src/main/java/adventofcode2021/day3/binarydiagnostic/DiagnosticReport.java
+++ src/main/java/adventofcode2021/day3/binarydiagnostic/DiagnosticReport.java
@@ -1,46 +1,14 @@
 package adventofcode2021.day3.binarydiagnostic;
 
+import adventofcode2021.collections.Pair;
+
 import java.util.List;
 
 import static java.util.Arrays.copyOf;
 
 public class DiagnosticReport {
-    private final List<Integer> values;
-    private final int[] bitCounts;
-    private final int gamma;
-    private final int epsilon;
 
-    public DiagnosticReport(final List<Integer> values) {
-        this.values = values;
-        this.bitCounts = countBits();
-
-        // Compute gamma and epsilon.
-        var m = 1;
-        var gamma = 0;
-        var epsilon = 0;
-        final var mid = values.size() / 2;
-        for (var bitCount : bitCounts) {
-            if (bitCount >= mid) gamma += m;
-            else epsilon += m;
-            m <<= 1;
-        }
-        this.gamma = gamma;
-        this.epsilon = epsilon;
-    }
-
-    public int[] getBitCounts() {
-        return bitCounts;
-    }
-
-    public int getGamma() {
-        return gamma;
-    }
-
-    public int getEpsilon() {
-        return epsilon;
-    }
-
-    private int[] countBits() {
+    public static int[] getBitCounts(final List<Integer> values) {
         var counts = new int[0];
         for (final var value : values) {
             for (int i = 0, r = value; r != 0; i++, r >>= 1) {
@@ -53,5 +21,19 @@ public class DiagnosticReport {
             }
         }
         return counts;
+    }
+
+    public static Pair<Integer, Integer> getGammaAndEpsilon(final List<Integer> values) {
+        final var bitCounts = getBitCounts(values);
+        var m = 1;
+        var gamma = 0;
+        var epsilon = 0;
+        final var mid = values.size() / 2;
+        for (var bitCount : bitCounts) {
+            if (bitCount >= mid) gamma += m;
+            else epsilon += m;
+            m <<= 1;
+        }
+        return new Pair<>(gamma, epsilon);
     }
 }
--- src/test/java/adventofcode2021/day3/binarydiagnostic/DiagnosticReportTests.java
+++ src/test/java/adventofcode2021/day3/binarydiagnostic/DiagnosticReportTests.java
@@ -7,6 +7,8 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static adventofcode2021.day3.binarydiagnostic.DiagnosticReport.getBitCounts;
+import static adventofcode2021.day3.binarydiagnostic.DiagnosticReport.getGammaAndEpsilon;
 import static java.nio.file.Files.readAllLines;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -17,31 +19,31 @@ public class DiagnosticReportTests {
 
     @Test
     public void shouldCountBits() {
-        final var diagnostic =
-                new DiagnosticReport(
-                        List.of(
-                                0b00100,
-                                0b11110,
-                                0b10110,
-                                0b10111,
-                                0b10101,
-                                0b01111,
-                                0b00111,
-                                0b11100,
-                                0b10000,
-                                0b11001,
-                                0b00010,
-                                0b01010));
+        final var values = List.of(
+                0b00100,
+                0b11110,
+                0b10110,
+                0b10111,
+                0b10101,
+                0b01111,
+                0b00111,
+                0b11100,
+                0b10000,
+                0b11001,
+                0b00010,
+                0b01010);
 
-        assertThat(diagnostic.getBitCounts(), is(equalTo(new int[]{5, 7, 8, 5, 7})));
-        assertThat(diagnostic.getGamma(), is(equalTo(22)));
-        assertThat(diagnostic.getEpsilon(), is(equalTo(9)));
+        assertThat(getBitCounts(values), is(equalTo(new int[]{5, 7, 8, 5, 7})));
+
+        final var pair = getGammaAndEpsilon(values);
+        assertThat(pair.getFirst(), is(equalTo(22)));
+        assertThat(pair.getSecond(), is(equalTo(9)));
     }
 
     @Test
     public void shouldReadDiagnosticFile() throws IOException {
-        final var diagnostic = new DiagnosticReport(parseDiagnosticsFile());
-        assertThat(diagnostic.getGamma() * diagnostic.getEpsilon(), is(equalTo(841526)));
+        final var pair = getGammaAndEpsilon(parseDiagnosticsFile());
+        assertThat(pair.getFirst() * pair.getSecond(), is(equalTo(841526)));
     }
 
     private static List<Integer> parseDiagnosticsFile() throws IOException {
