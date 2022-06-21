1f152c422522b15fdea85d71d66848016b2a71f7
Kais Dukes <dukeskais79@gmail.com>
2022-06-21 17:25:38 +0100

    Play bingo file.

--- src/test/java/adventofcode2021/day4/giantsquid/BingoTests.java
+++ src/test/java/adventofcode2021/day4/giantsquid/BingoTests.java
@@ -10,14 +10,24 @@ import static org.hamcrest.Matchers.is;
 
 public class BingoTests {
     private static final String EXAMPLE_FILE = "data/day4/example.txt";
+    private static final String BINGO_FILE = "data/day4/bingo.txt";
 
     @Test
     public void shouldPlayExampleFile() throws IOException {
-        final var bingoData = new BingoData(EXAMPLE_FILE);
+        assertThat(play(EXAMPLE_FILE), is(equalTo(4512)));
+    }
+
+    @Test
+    public void shouldPlayBingoFile() throws IOException {
+        assertThat(play(BINGO_FILE), is(equalTo(63424)));
+    }
+
+    private static int play(final String path) throws IOException {
+        final var bingoData = new BingoData(path);
         final var bingo = new Bingo(bingoData);
         final var pair = bingo.play();
         final var value = pair.getFirst();
         final var board = pair.getSecond();
-        assertThat(value * board.getScore(), is(equalTo(4512)));
+        return value * board.getScore();
     }
 }
