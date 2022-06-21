514f41959c9f15cbe115561e99f76d75955db840
Kais Dukes <dukeskais79@gmail.com>
2022-06-21 14:48:42 +0100

    Get gas ratings.

--- src/main/java/adventofcode2021/day3/binarydiagnostic/DiagnosticReport.java
+++ src/main/java/adventofcode2021/day3/binarydiagnostic/DiagnosticReport.java
@@ -2,20 +2,30 @@ package adventofcode2021.day3.binarydiagnostic;
 
 import adventofcode2021.collections.Pair;
 
+import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.Arrays.copyOf;
 
 public class DiagnosticReport {
 
+    public static int getMaxBits(final List<Integer> values) {
+        var max = 0;
+        for (final var value : values) {
+            var n = 0;
+            var r = value;
+            while (r != 0) {
+                n++;
+                r >>= 1;
+            }
+            if (n > max) max = n;
+        }
+        return max;
+    }
+
     public static int[] getBitCounts(final List<Integer> values) {
-        var counts = new int[0];
+        final var counts = new int[getMaxBits(values)];
         for (final var value : values) {
             for (int i = 0, r = value; r != 0; i++, r >>= 1) {
                 if ((r & 1) == 1) {
-                    if (counts.length <= i) {
-                        counts = copyOf(counts, i + 1);
-                    }
                     counts[i]++;
                 }
             }
@@ -35,5 +45,26 @@ public class DiagnosticReport {
             m <<= 1;
         }
         return new Pair<>(gamma, epsilon);
+    }
+
+    public static int getGasRating(final List<Integer> values, final boolean oxygen) {
+        final var list = new ArrayList<>(values);
+        final var maxBits = getMaxBits(list);
+        var mask = 1 << (maxBits - 1);
+        while (list.size() != 1) {
+            var count0 = 0;
+            var count1 = 0;
+            for (final var value : list) {
+                if ((value & mask) != 0) count1++;
+                else count0++;
+            }
+            final var v = oxygen
+                    ? count1 >= count0 ? mask : 0
+                    : count1 < count0 ? mask : 0;
+            final var m = mask;
+            list.removeIf(x -> (x & m) != v);
+            mask >>= 1;
+        }
+        return list.get(0);
     }
 }
--- src/test/java/adventofcode2021/day3/binarydiagnostic/DiagnosticReportTests.java
+++ src/test/java/adventofcode2021/day3/binarydiagnostic/DiagnosticReportTests.java
@@ -7,8 +7,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static adventofcode2021.day3.binarydiagnostic.DiagnosticReport.getBitCounts;
-import static adventofcode2021.day3.binarydiagnostic.DiagnosticReport.getGammaAndEpsilon;
+import static adventofcode2021.day3.binarydiagnostic.DiagnosticReport.*;
 import static java.nio.file.Files.readAllLines;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -16,34 +15,51 @@ import static org.hamcrest.Matchers.is;
 
 public class DiagnosticReportTests {
     private static final String DIAGNOSTIC_FILE = "data/day3/diagnostic.txt";
+    private static final List<Integer> TEST_VALUES = List.of(
+            0b00100,
+            0b11110,
+            0b10110,
+            0b10111,
+            0b10101,
+            0b01111,
+            0b00111,
+            0b11100,
+            0b10000,
+            0b11001,
+            0b00010,
+            0b01010);
+
+    @Test
+    public void shouldGetMaxBits() {
+        assertThat(getMaxBits(TEST_VALUES), is(equalTo(5)));
+    }
 
     @Test
     public void shouldCountBits() {
-        final var values = List.of(
-                0b00100,
-                0b11110,
-                0b10110,
-                0b10111,
-                0b10101,
-                0b01111,
-                0b00111,
-                0b11100,
-                0b10000,
-                0b11001,
-                0b00010,
-                0b01010);
+        assertThat(getBitCounts(TEST_VALUES), is(equalTo(new int[]{5, 7, 8, 5, 7})));
+    }
 
-        assertThat(getBitCounts(values), is(equalTo(new int[]{5, 7, 8, 5, 7})));
-
-        final var pair = getGammaAndEpsilon(values);
+    @Test
+    public void shouldGetGammaAndEpsilon() {
+        final var pair = getGammaAndEpsilon(TEST_VALUES);
         assertThat(pair.getFirst(), is(equalTo(22)));
         assertThat(pair.getSecond(), is(equalTo(9)));
     }
 
     @Test
-    public void shouldReadDiagnosticFile() throws IOException {
+    public void shouldGetGammaAndEpsilonFromDiagnosticFile() throws IOException {
         final var pair = getGammaAndEpsilon(parseDiagnosticsFile());
         assertThat(pair.getFirst() * pair.getSecond(), is(equalTo(841526)));
+    }
+
+    @Test
+    public void shouldGetOxygenRating() {
+        assertThat(getGasRating(TEST_VALUES, true), is(23));
+    }
+
+    @Test
+    public void shouldGetCO2Rating() {
+        assertThat(getGasRating(TEST_VALUES, false), is(10));
     }
 
     private static List<Integer> parseDiagnosticsFile() throws IOException {
