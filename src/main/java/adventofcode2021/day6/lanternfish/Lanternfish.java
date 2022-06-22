2dec811577e4c56719e5454b4f853b2da06192ad
Kais Dukes <dukeskais79@gmail.com>
2022-06-22 12:30:32 +0100

    Use less memory.

--- src/main/java/adventofcode2021/day6/lanternfish/Lanternfish.java
+++ src/main/java/adventofcode2021/day6/lanternfish/Lanternfish.java
@@ -1,19 +1,23 @@
 package adventofcode2021.day6.lanternfish;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Lanternfish {
 
-    public static void getNextDay(final List<Integer> timers) {
-        final var n = timers.size();
-        for (var i = 0; i < n; i++) {
-            final var timer = timers.get(i);
-            if (timer == 0) {
-                timers.set(i, 6);
-                timers.add(8);
-            } else {
-                timers.set(i, timer - 1);
-            }
+    public static long countFish(final List<Integer> timers, final int days) {
+        final var fish = new long[10];
+        for (final var timer : timers) {
+            fish[timer]++;
         }
+        for (var i = 0; i < days; i++) {
+            fish[7] += fish[0];
+            fish[9] = fish[0];
+            for (var j = 0; j < 9; j++) {
+                fish[j] = fish[j + 1];
+            }
+            fish[9] = 0;
+        }
+        return Arrays.stream(fish).sum();
     }
 }
--- src/test/java/adventofcode2021/day6/lanternfish/LanternfishTests.java
+++ src/test/java/adventofcode2021/day6/lanternfish/LanternfishTests.java
@@ -5,12 +5,11 @@ import org.junit.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static adventofcode2021.day6.lanternfish.Lanternfish.getNextDay;
-import static java.util.stream.Collectors.toCollection;
+import static adventofcode2021.day6.lanternfish.Lanternfish.countFish;
+import static java.util.stream.Collectors.toList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
@@ -20,22 +19,16 @@ public class LanternfishTests {
 
     @Test
     public void shouldCountFishForExampleData() {
-        final var timers = new ArrayList<>(List.of(3, 4, 3, 1, 2));
-        for (var i = 0; i < 80; i++) {
-            getNextDay(timers);
-        }
-        assertThat(timers.size(), is(equalTo(5934)));
+        final var timers = List.of(3, 4, 3, 1, 2);
+        assertThat(countFish(timers, 80), is(equalTo(5934L)));
     }
 
     @Test
     public void shouldCountFish() throws IOException {
         final var timers = Arrays.stream(
                         Files.readString(Paths.get(LANTERNFISH_FILE)).split(","))
-                .map(Integer::parseInt).collect(toCollection(ArrayList::new));
+                .map(Integer::parseInt).collect(toList());
 
-        for (var i = 0; i < 80; i++) {
-            getNextDay(timers);
-        }
-        assertThat(timers.size(), is(equalTo(386640)));
+        assertThat(countFish(timers, 80), is(equalTo(386640L)));
     }
 }
